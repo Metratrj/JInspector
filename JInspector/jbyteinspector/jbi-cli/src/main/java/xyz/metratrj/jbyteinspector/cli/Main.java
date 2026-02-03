df52604e54f84513b0b5279ab55541ae0526582d
Johannes Grimm <grimmjohannes1998@gmail.com>
2026-02-03 08:52:47 +0100

    rename in @ClassFile.java logger to LOGGER for consistency For testing purpose set static analysis path

--- JInspector/jbyteinspector/jbi-cli/src/main/java/xyz/metratrj/jbyteinspector/cli/Main.java
+++ JInspector/jbyteinspector/jbi-cli/src/main/java/xyz/metratrj/jbyteinspector/cli/Main.java
@@ -12,30 +12,32 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        if (args.length == 0) {
+        /*if (args.length == 0) {
             System.out.println("Usage: JByteInspector <path-to-classes>");
             return;
-        }
+        }*/
 
-        Path path = Paths.get(args[0]);
-        System.out.println("Inspecting: " + path.toAbsolutePath());
+        //Path path  = Paths.get(args[0]);
+        Path oPath = Paths.get("/home/metratrj/sources/JInspector/Code/JInspector/out/production/TestModule/xyz/metratrj");
 
-        AnalysisService service = new JByteInspectorEngine();
-        List<ClassReport> reports = service.analyze(path);
+        System.out.println("Inspecting: " + oPath.toAbsolutePath());
+
+        AnalysisService   service = new JByteInspectorEngine();
+        List<ClassReport> reports = service.analyze(oPath);
 
         for (ClassReport report : reports) {
             System.out.println("--------------------------------------------------");
             System.out.println("Class: " + report.className());
             System.out.println("Super: " + report.superClassName());
             System.out.println("Flags: " + report.flags());
-            
+
             if (!report.fields().isEmpty()) {
                 System.out.println("\nFields:");
                 for (FieldReport f : report.fields()) {
                     System.out.printf("  %s %s\n", f.flags(), f.name());
                 }
             }
-            
+
             if (!report.methods().isEmpty()) {
                 System.out.println("\nMethods:");
                 for (MethodReport m : report.methods()) {
